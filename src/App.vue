9175df647d770c95c60f2f96c82367cad3fbf713
Jonathan <jonathankonstantin@icloud.com>
2026-08-25 18:31:11 +0200

    Live Preise anzeigen

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
-<script setup lang="ts"></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const preise = ref([])
+const ladeFehler = ref(null)
+
+async function ladeDaten() {
+  try {
+    const antwort = await fetch('http://178.105.153.4:3001/api/aktuelle-preise')
+    if (!antwort.ok) throw new Error('Serverfehler: ' + antwort.status)
+    preise.value = await antwort.json()
+  } catch (fehler) {
+    ladeFehler.value = fehler.message
+  }
+}
+
+onMounted(() => {
+  ladeDaten()
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <div>
+    <h1>Spread Monitor</h1>
 
-<style scoped></style>
+    <p v-if="ladeFehler">Fehler beim Laden: {{ ladeFehler }}</p>
+
+    <table v-else>
+      <thead>
+      <tr>
+        <th>ISIN</th>
+        <th>Börse</th>
+        <th>Preis</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="eintrag in preise" :key="eintrag.isin + eintrag.boerse">
+        <td>{{ eintrag.isin }}</td>
+        <td>{{ eintrag.boerse }}</td>
+        <td>{{ eintrag.aktueller_preis }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
