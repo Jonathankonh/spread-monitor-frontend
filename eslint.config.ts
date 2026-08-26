c26a46d3106371b8bf7aa41453140362b0becab8
Jonathan <jonathankonstantin@icloud.com>
2026-08-26 12:48:13 +0200

    Neue Design Struktur angelegt.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,10 @@
 import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs, configureVueProject } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
 import skipFormatting from 'eslint-config-prettier/flat'
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+configureVueProject({ scriptLangs: ['js'] })
 
 export default defineConfigWithVueTs(
   {
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs, configureVueProject } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginOxlint from 'eslint-plugin-oxlint'
-import skipFormatting from 'eslint-config-prettier/flat'
+<script setup lang="js">
+import { ref } from 'vue'
+import IsinAuswahl from './components/IsinAuswahl.vue'
+import VerlaufChart from './components/VerlaufChart.vue'
 
-configureVueProject({ scriptLangs: ['js'] })
+const aktuelleIsin = ref('')
 
-export default defineConfigWithVueTs(
-{
-name: 'app/files-to-lint',
-files: ['**/*.{vue,ts,mts,tsx}'],
-},
-globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
-...pluginVue.configs['flat/essential'],
-vueTsConfigs.recommended,
-...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
-skipFormatting,
-)
+function beiAuswahlGeaendert(isin) {
+  aktuelleIsin.value = isin
+}
+</script>
+
+<template>
+  <div style="max-width: 900px; margin: 0 auto; padding: 20px">
+    <h1>Spread Monitor</h1>
+    <IsinAuswahl @auswahl-geaendert="beiAuswahlGeaendert" />
+    <VerlaufChart v-if="aktuelleIsin" :isin="aktuelleIsin" />
+  </div>
+</template>
